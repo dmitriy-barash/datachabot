7ddeb2f15ff018998b3974545b1fa2f75eef5b36
dmitriy-barash <Dmitriy.Barash@globalaes.com>
2024-07-22 20:57:26 -0400

    Updated

--- datachatbot/main.py
+++ datachatbot/main.py
@@ -8,6 +8,12 @@
 # pip install numpy --upgrade
 # pip install faiss-cpu
 
+# pip install python-dotenv==1.0.0 langchain==0.0.302 unstructured==0.10.12 sentence_transformers==2.2.2 tqdm==4.66.1 faiss-cpu==1.7.4
+# pip install python-dotenv langchain unstructured sentence_transformers tqdm faiss-cpu
+# pip install python-dotenv -U langchain -U unstructured -U sentence_transformers -U tqdm -U faiss-cpu -U 
+# pip uninstall python-magic
+# pip install python-magic-bin==0.4.14
+
 import streamlit as st
 
 from chat import create_application
